918aeaf25ef3f4cd3be3cf012a9ed4bec121d1ec
ngarciaotero <nasleygarciaotero@gmail.com>
2024-02-17 14:47:15 -0500

    Track new script file

---
+++ login-form.js
@@ -0,0 +1,26 @@
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const phoneNumberInput = document.getElementById("phone-number");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+const inputs = document.querySelectorAll("input");
+const progressCircle = document.querySelector("circle");
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.checkValidity()) {
+      input.classList.remove("invalid");
+      input.classList.add("valid");
+    } else {
+      input.classList.remove("valid");
+      input.classList.add("invalid");
+    }
+  });
+});
+
+function updateProgress() {
+  const offset = 1570 - (percent / 100) * 1570;
+  progressCircle.style.strokeDashoffset = offset;
+  progressCircle.style.strokeWidth = 20;
+}

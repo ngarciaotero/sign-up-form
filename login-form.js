2f8f80f169c589fe11c6095ea92f2eeeb2551f55
ngarciaotero <nasleygarciaotero@gmail.com>
2024-02-19 23:27:50 -0500

    Refactor updateProgress() create helper function countValidInputs()

--- login-form.js
+++ login-form.js
@@ -14,14 +14,7 @@ inputs.forEach((input) => {
 });
 
 function updateProgress() {
-  let validInputs = 0;
-
-  inputs.forEach((input) => {
-    if (input.checkValidity()) {
-      validInputs++;
-    }
-  });
-  const percent = (validInputs / inputs.length) * 100;
+  const percent = (countValidInputs() / inputs.length) * 100;
   const offset = 1570 - (percent / 100) * 1570;
   progressCircle.style.strokeDashoffset = offset;
   progressCircle.style.strokeWidth = 20;
@@ -47,4 +40,14 @@ function validatePassword() {
 function enableCreateAccountBtn() {
   const createAccountBtn = document.querySelector(".create-account-btn");
   createAccountBtn.disabled = false;
+}
+
+function countValidInputs() {
+  let validInputs = 0;
+  inputs.forEach((input) => {
+    if (input.checkValidity()) {
+      validInputs++;
+    }
+  });
+  return validInputs;
 }
